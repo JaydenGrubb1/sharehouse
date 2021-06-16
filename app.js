8facac0bafed3701397104f936de138ae5c17037
Jayden Grubb <jaydengrubb1@gmail.com>
2021-06-17 01:32:55 +1000

    added basic endpoints and configured database

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const options = require('./knexfile');
+const knex = require('knex')(options);
+const helmet = require('helmet');
+const cors = require('cors');
 
-var app = express();
+const financeRouter = require('./routes/finances');
+const userRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+	req.knex = knex;
+	next();
+});
 
-app.use('/', indexRouter);
+app.use('/finances', financeRouter);
+app.use('/users', userRouter);
 
+app.use('/test', function (req, res, next) {
+	res.status(200).json({
+		error: false,
+		message: "Test successful!"
+	});
+});
 app.use(function (req, res, next) {
-    res.status(404).json({ status: "error", message: "Page not found!" });
+	res.status(404).json({
+		error: true,
+		message: "Page not found!"
+	});
 });
 
 module.exports = app;
