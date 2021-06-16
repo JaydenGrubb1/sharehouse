b253883d9c3e50512c80c71fd819c1aff8c4cd28
Jayden Grubb <jaydengrubb1@gmail.com>
2021-06-16 23:08:45 +1000

    express framework setup

---
+++ app.js
@@ -0,0 +1,22 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use(function (req, res, next) {
+    res.status(404).json({ status: "error", message: "Page not found!" });
+});
+
+module.exports = app;
