309b467c9360491b2dc50da881097a3d0c293f81
Jayden Grubb <jaydengrubb1@gmail.com>
2021-07-20 19:30:55 +1000

    refactored transactions into its own component

---
+++ src/components/payments/transactions.jsx
@@ -0,0 +1,84 @@
+import { faCaretDown, faCaretUp, faSync } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Button, Card, CardBody, CardHeader, Collapse, DropdownItem, DropdownMenu, DropdownToggle, Table, UncontrolledDropdown } from "reactstrap";
+import { getPayments } from "../../api";
+import Entry from "./entry";
+
+export default function Transactions(props) {
+	const [payments, setPayments] = useState(undefined);
+	const [receipts, setReceipts] = useState();
+
+	const [limit, setLimit] = useState(10);
+	const [filterOpen, setFilterOpen] = useState(false);
+
+	const toggleFilter = () => setFilterOpen(!filterOpen);
+
+	async function getData() {
+		let results = await getPayments(undefined, undefined, undefined, limit, 0);
+		if (results.error) {
+			props.error(results.message);
+		} else {
+			if (results.data && results.data.length > 0) {
+				setPayments(results.data);
+			}
+		}
+		setReceipts();
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (<Card className="mt-3">
+		<CardHeader className="d-flex">
+			<h5 className="my-auto">Past Transactions</h5>
+			<Button className="ml-auto my-auto" outline onClick={getData}>
+				<FontAwesomeIcon icon={faSync} />
+			</Button>
+		</CardHeader>
+		<CardBody className="p-0">
+			<div className="p-3">
+				<Button color="primary" onClick={toggleFilter}>
+					Filter <FontAwesomeIcon icon={filterOpen ? faCaretUp : faCaretDown} />
+				</Button>
+				<Collapse isOpen={filterOpen}>
+					<div className="mt-3">
+						<UncontrolledDropdown>
+							<DropdownToggle color="primary">
+								Limit {limit} <FontAwesomeIcon icon={faCaretDown} />
+							</DropdownToggle>
+							<DropdownMenu>
+								<DropdownItem onClick={() => setLimit(10)}>10</DropdownItem>
+								<DropdownItem onClick={() => setLimit(20)}>20</DropdownItem>
+								<DropdownItem onClick={() => setLimit(40)}>40</DropdownItem>
+								<DropdownItem onClick={() => setLimit(60)}>60</DropdownItem>
+								<DropdownItem onClick={() => setLimit(80)}>80</DropdownItem>
+								<DropdownItem onClick={() => setLimit(100)}>100</DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+					</div>
+				</Collapse>
+			</div>
+			<Table className="my-0" responsive>
+				<tbody>
+					{payments &&
+						payments.map(x => {
+							return <Entry data={x} />
+						})
+					}
+				</tbody>
+			</Table>
+			<Table className="my-0" responsive>
+				<tbody>
+					{receipts &&
+						receipts.map(x => {
+							return <Entry data={x} />
+						})
+					}
+				</tbody>
+			</Table>
+		</CardBody>
+	</Card>
+	);
+}
--- src/pages/payments.jsx
+++ src/pages/payments.jsx
@@ -1,11 +1,11 @@
-import { faCaretDown, faCaretUp, faQuestionCircle, faSync } from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
-import { Button, Card, CardBody, CardDeck, CardHeader, CardSubtitle, CardText, Collapse, DropdownItem, DropdownMenu, DropdownToggle, Table, UncontrolledAlert, UncontrolledDropdown, UncontrolledTooltip } from "reactstrap";
-import { getDebt, getPayments, getTotalDebt } from "../api";
-import Entry from "../components/payments/entry";
+import { Button, Card, CardBody, CardDeck, CardHeader, CardSubtitle, CardText, Collapse, UncontrolledAlert, UncontrolledTooltip } from "reactstrap";
+import { getDebt, getTotalDebt } from "../api";
 import Pending from "../components/payments/pending";
+import Transactions from "../components/payments/transactions";
 
 export default function Payments() {
 
@@ -13,13 +13,6 @@ export default function Payments() {
 
 	const [debt, setDebt] = useState(0);
 	const [totalDebt, setTotalDebt] = useState(0);
-	const [payments, setPayments] = useState(undefined);
-	const [receipts, setReceipts] = useState();
-
-	const [limit, setLimit] = useState(10);
-	const [tFilterOpen, setTFilterOpen] = useState(false);
-
-	const toggleTFilter = () => setTFilterOpen(!tFilterOpen);
 
 	async function getDebts() {
 		let results = await getDebt();
@@ -44,21 +37,8 @@ export default function Payments() {
 		setTotalDebt(total);
 	}
 
-	async function getTransactions() {
-		let results = await getPayments(undefined, undefined, undefined, limit, 0);
-		if (results.error) {
-			setError(results.message);
-		} else {
-			if (results.data && results.data.length > 0) {
-				setPayments(results.data);
-			}
-		}
-		setReceipts();
-	}
-
 	useEffect(() => {
 		getDebts();
-		getTransactions();
 	}, []);
 
 	return (
@@ -98,56 +78,7 @@ export default function Payments() {
 				</Card>
 			</CardDeck>
 			<Pending error={setError} />
-			<Card className="mt-3">
-				<CardHeader className="d-flex">
-					<h5 className="my-auto">Past Transactions</h5>
-					<Button className="ml-auto my-auto" outline onClick={getTransactions}>
-						<FontAwesomeIcon icon={faSync} />
-					</Button>
-				</CardHeader>
-				<CardBody className="p-0">
-					<div className="p-3">
-						<Button color="primary" onClick={toggleTFilter}>
-							Filter <FontAwesomeIcon icon={tFilterOpen ? faCaretUp : faCaretDown} />
-						</Button>
-						<Collapse isOpen={tFilterOpen}>
-							<div className="mt-3">
-								<UncontrolledDropdown>
-									<DropdownToggle color="primary">
-										Limit {limit} <FontAwesomeIcon icon={faCaretDown} />
-									</DropdownToggle>
-									<DropdownMenu>
-										<DropdownItem onClick={() => setLimit(10)}>10</DropdownItem>
-										<DropdownItem onClick={() => setLimit(20)}>20</DropdownItem>
-										<DropdownItem onClick={() => setLimit(40)}>40</DropdownItem>
-										<DropdownItem onClick={() => setLimit(60)}>60</DropdownItem>
-										<DropdownItem onClick={() => setLimit(80)}>80</DropdownItem>
-										<DropdownItem onClick={() => setLimit(100)}>100</DropdownItem>
-									</DropdownMenu>
-								</UncontrolledDropdown>
-							</div>
-						</Collapse>
-					</div>
-					<Table className="my-0" responsive>
-						<tbody>
-							{payments &&
-								payments.map(x => {
-									return <Entry data={x} />
-								})
-							}
-						</tbody>
-					</Table>
-					<Table className="my-0" responsive>
-						<tbody>
-							{receipts &&
-								receipts.map(x => {
-									return <Entry data={x} />
-								})
-							}
-						</tbody>
-					</Table>
-				</CardBody>
-			</Card>
+			<Transactions error={setError} />
 		</div>
 	);
 }
