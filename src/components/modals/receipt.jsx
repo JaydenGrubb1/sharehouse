7e86d485d9b57c4d6fb2845e07e0760a2c34e9f2
Jayden Grubb <jaydengrubb1@gmail.com>
2021-08-03 15:54:07 +1000

    implemented timestamp option for receipts

--- src/components/modals/receipt.jsx
+++ src/components/modals/receipt.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import dateFormat from "dateformat";
+import React, { useState, useEffect } from "react";
 import { Button, Collapse, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Label, Modal, ModalBody, ModalFooter, ModalHeader, Spinner } from "reactstrap";
 import { createReceipt, getEmail } from "../../api";
 
@@ -9,13 +10,15 @@ export default function Receipt(props) {
 	const [showDatetime, setShowDatetime] = useState(false);
 	const [store, setStore] = useState();
 	const [amount, setAmount] = useState();
+	const [date, setDate] = useState();
+	const [time, setTime] = useState();
 
 	async function onSubmit() {
 		setLoading(true);
 		let details = {
-			"user": getEmail(),
 			"store": store,
-			"cost": amount
+			"cost": amount,
+			"timestamp": "".concat(date, " ", time)
 		};
 		let results = await createReceipt(details);
 		if (results.error) {
@@ -26,6 +29,12 @@ export default function Receipt(props) {
 		}
 		setLoading(false);
 	}
+
+	useEffect(() => {
+		setShowDatetime(false);
+		setDate(dateFormat(new Date(), "yyyy-mm-dd"));
+		setTime(dateFormat(new Date(), "HH:MM"));
+	}, [props.open]);
 
 	return (
 		<div>
@@ -48,21 +57,23 @@ export default function Receipt(props) {
 						</FormGroup>
 						<FormGroup check>
 							<Label check>
-								<Input type="checkbox" checked={!showDatetime} />Use current date and time
+								<Input type="checkbox" checked={!showDatetime} onChange={x => setShowDatetime(!x.target.checked)} />Use current date and time
 							</Label>
 						</FormGroup>
 						<Collapse isOpen={showDatetime}>
 							<FormGroup>
 								<Label for="datefield">Date</Label>
-								<Input type="date" name="date" id="datefield" />
+								<Input type="date" name="date" id="datefield" value={date} onChange={x => setDate(x.target.value)} />
 							</FormGroup>
 							<FormGroup>
 								<Label for="timefield">Time</Label>
-								<Input type="time" name="time" id="timefield" />
+								<Input type="time" name="time" id="timefield" value={time} onChange={x => setTime(x.target.value)} />
 							</FormGroup>
 						</Collapse>
 					</Form>
-					<p className="text-danger text-center mt-3">{error}</p>
+					{error &&
+						<p className="text-danger text-center mt-3">{error}</p>
+					}
 				</ModalBody>
 				<ModalFooter>
 					{loading &&
