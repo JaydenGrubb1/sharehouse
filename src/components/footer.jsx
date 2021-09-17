4838f9120275b4a3e2c9070768e5ffad9a122fa1
Jayden Grubb <jaydengrubb1@gmail.com>
2021-09-18 02:18:15 +1000

    footer font size change

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,13 +8,13 @@ export default function Footer() {
 		<footer className="footer">
 			<div className="container d-flex h-100">
 				<div className="my-auto w-100 d-flex">
-					<span className="text-muted">&copy; Copyright 2021 Jayden Grubb</span>
+					<span className="text-muted my-auto">&copy; Copyright 2021 Jayden Grubb</span>
 					{/* <span className="ml-auto"><a className="text-muted" href="mailto:contact@jaydengrubb.com">contact@jaydengrubb.com</a></span> */}
-					<a className="ml-auto" href="https://github.com/JaydenGrubb1" target="_blank"><FontAwesomeIcon className="text-muted" icon={faGithub} /></a>
-					<a className="ml-1" href="https://www.linkedin.com/in/jayden-grubb-ab162a123/" target="_blank"><FontAwesomeIcon className="text-muted" icon={faLinkedin} /></a>
-					<a className="ml-1" href="https://twitter.com/JaydenGrubb" target="_blank"><FontAwesomeIcon className="text-muted" icon={faTwitter} /></a>
-					<a className="ml-1" href="mailto:contact@jaydengrubb.com"><FontAwesomeIcon className="text-muted" icon={faEnvelope} /></a>
-					<a className="ml-1" href="https://www.jaydengrubb.com/" target="_blank"><FontAwesomeIcon className="text-muted" icon={faHome} /></a>
+					<a className="ml-auto my-auto h4" href="https://github.com/JaydenGrubb1" target="_blank"><FontAwesomeIcon className="text-muted" icon={faGithub} /></a>
+					<a className="ml-1 my-auto h4" href="https://www.linkedin.com/in/jayden-grubb-ab162a123/" target="_blank"><FontAwesomeIcon className="text-muted" icon={faLinkedin} /></a>
+					<a className="ml-1 my-auto h4" href="https://twitter.com/JaydenGrubb" target="_blank"><FontAwesomeIcon className="text-muted" icon={faTwitter} /></a>
+					<a className="ml-1 my-auto h4" href="mailto:contact@jaydengrubb.com"><FontAwesomeIcon className="text-muted" icon={faEnvelope} /></a>
+					<a className="ml-1 my-auto h4" href="https://www.jaydengrubb.com/" target="_blank"><FontAwesomeIcon className="text-muted" icon={faHome} /></a>
 				</div>
 			</div>
 		</footer>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,13 +9,17 @@ export default function Home() {
 				<title>Sharehouse</title>
 			</Helmet>
 			<Jumbotron fluid>
-				<h1 className="display-3">Hello, world!</h1>
-				<p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+				<div className="p-3">
+					<h1 className="display-3">Hello, world!</h1>
+					<p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+				</div>
 				<hr className="my-2" />
-				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-				<p className="lead">
-					<Button color="primary">Learn More</Button>
-				</p>
+				<div className="p-3">
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<p className="lead">
+						<Button color="primary">Learn More</Button>
+					</p>
+				</div>
 			</Jumbotron>
 		</div>
 	);
