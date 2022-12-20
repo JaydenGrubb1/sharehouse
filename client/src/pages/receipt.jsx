b3e7f37134f4f8832936b22f87f215f5013d1249
Jayden Grubb <jaydengrubb1@gmail.com>
2022-12-20 22:21:10 +1000

    added receipts details ui minus contributions

--- client/src/pages/receipt.jsx
+++ client/src/pages/receipt.jsx
@@ -1,21 +1,18 @@
+import dateFormat from "dateformat";
 import { faL } from "@fortawesome/free-solid-svg-icons";
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
-import { Alert, Card, CardBody, CardDeck, CardHeader, CardImg, CardText, Collapse, UncontrolledAlert } from "reactstrap";
+import { Alert, Card, CardBody, CardDeck, CardHeader, CardImg, CardText, Col, Collapse, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Label, Row, UncontrolledAlert } from "reactstrap";
 import { getReceipt, getServerRoot, hashInteger } from "../api";
 
 export default function User() {
 	const { id } = useParams();
 	const [loaded, setLoaded] = useState(true);
-
-	const idInt = parseInt(id);
-	// const hashID = hashInteger(idInt);
-
 	const [data, setData] = useState();
 
 	async function getDetails() {
-		let results = await getReceipt(idInt);
+		let results = await getReceipt(parseInt(id));
 
 		if (results.error) {
 			// TODO handle error
@@ -42,13 +39,69 @@ export default function User() {
 					<CardHeader className="d-flex">
 						<h5 className="my-auto">Details</h5>
 					</CardHeader>
-					<CardBody>
+					{/* <CardBody>
 						<CardText>
 							<samp style={{whiteSpace: "pre-wrap"}}>
 								{JSON.stringify(data, null, 2)}
 							</samp>
+
 						</CardText>
-					</CardBody>
+					</CardBody> */}
+					{data &&
+						<Form className="p-3">
+							<FormGroup>
+								<Label for="userfield">User</Label>
+								<Input id="userfield" disabled value={data.user} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="storefield">Store</Label>
+								<Input id="storefield" disabled value={data.store} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="locationfield">Location</Label>
+								<Input id="locationfield" disabled value={data.location} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="descriptionfield">Description</Label>
+								<Input id="descriptionfield" disabled value={data.description} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="amountfield">Amount</Label>
+								<InputGroup>
+									<InputGroupAddon addonType="prepend">
+										<InputGroupText>$</InputGroupText>
+									</InputGroupAddon>
+									<Input id="amountfield" disabled value={data.amount} />
+								</InputGroup>
+							</FormGroup>
+							<Label for="receiptfield">Receipt Timestamp</Label>
+							<Row id="receiptfield" form>
+								<Col>
+									<FormGroup>
+										<Input type="date" disabled required value={dateFormat(data.timestamp, "yyyy-mm-dd")} />
+									</FormGroup>
+								</Col>
+								<Col>
+									<FormGroup>
+										<Input type="time" disabled required value={dateFormat(data.timestamp, "HH:MM")} />
+									</FormGroup>
+								</Col>
+							</Row>
+							<Label for="recordfield">Record Timestamp</Label>
+							<Row id="recordfield" form>
+								<Col>
+									<FormGroup>
+										<Input type="date" disabled required value={dateFormat(data.record_timestamp, "yyyy-mm-dd")} />
+									</FormGroup>
+								</Col>
+								<Col>
+									<FormGroup>
+										<Input type="time" disabled required value={dateFormat(data.record_timestamp, "HH:MM")} />
+									</FormGroup>
+								</Col>
+							</Row>
+						</Form>
+					}
 				</Card>
 				<Card>
 					<CardHeader className="d-flex">
